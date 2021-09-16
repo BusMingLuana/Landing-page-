3fa5da8c7e6e6a9c7c2b9da66eb382ae0feb84d6
luana mingione <mingluana2020@gmail.com>
2021-09-16 20:26:03 -0300

    landing page

---
+++ js/menu.js
@@ -0,0 +1,45 @@
+
+/// Cuadro horizontal del menu
+
+	var cuadroMenu = document.getElementById("cuadro-menu");
+
+	//window.onscroll = function(){
+	  var anchoWindow = $(window).width();
+	  if (anchoWindow > 800) 
+	  {
+	  	window.onscroll = function(){
+		if (window.pageYOffset > 100) {
+			cuadroMenu.style.background = "#000";
+		   } else {
+			cuadroMenu.style.background = "transparent0";
+		   }
+		}
+	  } else {
+	  		cuadroMenu.style.background = "#000";
+	  }
+	//}
+
+
+/// Cuadro vertical del menu 
+$(document).ready(menu);
+
+var contador = 1;
+
+function menu(){
+	$("#iconoMenu2").click(function(){
+		
+		if (contador == 1) {
+			$(".menu").animate({
+				left: "0"
+			});
+			contador = 0;
+		} else {
+			$(".menu").animate({
+				left: "-100%"
+			});
+			contador = 1;
+		}
+	
+	});
+
+}
